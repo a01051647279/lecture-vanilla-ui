df11529644ebab32514346cdba34b34825823f4f
Tae Hyun Kim <a01051647279@gmail.com>
2023-07-20 00:42:57 +0900

    Add: FormView-4
    입력 폼 검색어가 유무와 x-버튼 클릭 시 x-버튼 숨김 처리

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -6,9 +6,14 @@ export default {
   init() {
     FormView.setup(document.querySelector('form')) //
       .on('@submit', (e) => this.onSubmit(e.detail.input))
+      .on('@reset', () => this.onReset())
   },
 
   onSubmit(e) {
+    debugger
+  },
+
+  onReset() {
     debugger
   },
 }
--- js/views/FormView.js
+++ js/views/FormView.js
@@ -21,13 +21,20 @@ FormView.showResetBtn = function (show) {
 FormView.bindEvents = function () {
   this.el.addEventListener('submit', (e) => e.preventDefault())
   this.inputEl.addEventListener('keyup', (e) => this.handleKeyup(e))
+  this.resetEl.addEventListener('click', () => this.handleClick())
 }
 
 FormView.handleKeyup = function (e) {
   this.showResetBtn(e.target.value)
+  if (!e.target.value) this.emit('@reset')
   if (e.key !== 'Enter') return
   if (!e.target.value.trim()) return
   this.emit('@submit', { input: e.target.value.trim() })
+}
+
+FormView.handleClick = function () {
+  this.emit('@reset')
+  this.showResetBtn(false)
 }
 
 export default FormView
